d9a5bcf8f6772ed89ca81a58d545d562fd989660
Matias <alexisbordones18@gmail.com>
2026-06-06 12:22:00 -0400

    Corregir rutas y apuntar servidor a src/app.js

--- backend-galeria-fotos/src/app.js
+++ backend-galeria-fotos/src/app.js
@@ -21,11 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Esto permite que el servidor entregue tu index.html y cualquier archivo CSS/JS en esa ruta
-app.use(express.static(path.join(__dirname, '../../')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // Cuando alguien entre a la ruta principal '/', le enviamos el HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.use('/uploads', express.static(path.resolve(process.cwd(), env.uploadDir)));
